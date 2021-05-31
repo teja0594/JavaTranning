2c66228550f6869ad1e6430941dabf81b85e4a46
170030594 <170030594@kluniversity.in>
2021-05-31 21:08:11 +0530

    Commit

---
+++ corejavatranning/src/com/vm/tranning/java/strings/StringBufferDemo.java
@@ -0,0 +1,10 @@
+package com.vm.tranning.java.strings;
+
+public class StringBufferDemo {
+
+public static void main(String[] args) {
+	
+  StringBuffer sbuf = new StringBuffer("Value Momentum");
+  System.out.println(sbuf);
+}
+}
